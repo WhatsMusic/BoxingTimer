48e63fc9f1ddae89ca13265701bd5fcb5245e420
WhatsMusic.de <info@whatsmusic.de>
2023-06-12 16:52:57 +0200

    added class BoxingTimer & TrainingPlan

---
+++ src/classes/BoxingTimer.kt
@@ -0,0 +1,6 @@
+class BoxingTimer(val trainingPlans: MutableList<TrainingPlan>) {
+
+        fun selectTrainingPlan(plan: TrainingPlan) {
+            //TODO: TrainingPlan wählen implementieren
+        }
+}
